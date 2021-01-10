ae1a603609a2a5eb5e91ce3c196ccdabe94deef3
ITkah <ilatkacenko132@gmail.com>
2021-01-10 16:09:43 +0200

    vipadaha

--- js/common.js
+++ js/common.js
@@ -1,3 +1,20 @@
-jQuery('.partners_question_container, .partners_question_img').theiaStickySidebar({
-    additionalMarginTop: 25
-});
+//Header script
+var callLang = document.getElementsByClassName('top_panel_lang');
+var hideLang = document.getElementsByClassName('lang_hide');
+
+callLang[0].onclick = function() {
+    hideLang[0].classList.toggle("lang_hide_active");
+}
+
+var callVoice = document.getElementsByClassName('top_panel_notification');
+var callText = document.getElementsByClassName('notification_text');
+
+callVoice[0].onclick = function() {
+    if (callText[0].innerHTML === "Выкл") {
+        callText[0].innerHTML = "Вкл";
+    } else {
+        callText[0].innerHTML = "Выкл";
+    }
+}
+
+//Header end
