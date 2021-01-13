c4b0f890e3cc9ce469946521fce46fc46c82e07d
ITkah <ilatkacenko132@gmail.com>
2021-01-13 12:52:56 +0200

    fix_mod

--- js/common.js
+++ js/common.js
@@ -6,16 +6,11 @@ callLang[0].onclick = function() {
     hideLang[0].classList.toggle("lang_hide_active");
 }
 
-var callVoice = document.getElementsByClassName('top_panel_notification');
-var callText = document.getElementsByClassName('notification_text');
 
-callVoice[0].onclick = function() {
-    if (callText[0].innerHTML === "Выкл") {
-        callText[0].innerHTML = "Вкл";
-    } else {
-        callText[0].innerHTML = "Выкл";
-    }
-}
+$(".top_panel_notification").on("click", function() {
+    $(this).toggleClass("top_panel_notification_active");
+});
+
 
 var callMes = document.getElementsByClassName('message_img');
 var hideMes = document.getElementsByClassName('message_hide');
@@ -44,6 +39,7 @@ function copyRef() {
 $(".call_menu").on("click", function(e) {
     e.preventDefault();
     $(this).siblings('ul').slideToggle();
+    $(this).toggleClass("call_menu_active");
 });
 
 $(".header_btn_img").on("click", function() {
@@ -53,5 +49,7 @@ $(".header_btn_img").on("click", function() {
 $(".header_nav_close").on("click", function() {
     $(".header_nav").removeClass("header_nav_active");
 });
+
+
 
 //Header end
